09138de2b1361fc554960242dd9fde1df595d828
Perebonilado <richad.eradiri@cicod.com>
2022-11-24 19:09:18 +0100

    deleted unused fxn

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { login, signUp } = require("../controllers/auth");
 const { uploadImage } = require("../config/multer");
 const { signUpValidations, loginValidations } = require("../validations/auth");
-const { authGuard } = require("../middleware/authGuard");
 
 router.post("/login", ...loginValidations, login);
 
